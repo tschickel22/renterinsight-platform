c57aab6e9c7e5199a3b08946e49ea4245a93cca3
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-06-26 12:47:44 -0600

    Updated vite.config.js

--- apps/commission-engine/vite.config.js
+++ apps/commission-engine/vite.config.js
@@ -4,12 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
