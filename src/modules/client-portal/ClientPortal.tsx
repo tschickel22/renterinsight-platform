67c8a1540f4bbdd74ef05389ea07ab543535754c
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-06-27 10:29:14 -0600

    Fix ClientPortal import path

--- src/modules/client-portal/ClientPortal.tsx
+++ src/modules/client-portal/ClientPortal.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 
 // Re-export the ClientPortal component from the apps directory
-export { default } from '../../apps/client-portal/src/ClientPortal';
+export { default } from '../../../apps/client-portal/src/ClientPortal';
