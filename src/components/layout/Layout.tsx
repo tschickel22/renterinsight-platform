59d624989a3302769e2a3ff2fc02731e57f93a7e
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-06-29 21:14:51 -0600

    Updated Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from 'react'
+// src/components/layout/Layout.tsx
+import React, { ReactNode, useState } from 'react'
 import Sidebar from './Sidebar'
 import Header from './Header'
 
@@ -7,11 +8,13 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
   return (
     <div className="min-h-screen bg-background">
-      <Sidebar />
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className="lg:pl-64">
-        <Header />
+        <Header onMenuButtonClick={() => setSidebarOpen(true)} />
         <main className="py-6">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             {children}
