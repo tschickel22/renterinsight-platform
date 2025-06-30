27efb01c49d01bde41ebffb7137b918e048447ce
tschickel22 <109991351+tschickel22@users.noreply.github.com>
2025-06-30 10:51:12 -0600

    Deleted supabase.ts

--- src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js'
-
-// Create a single supabase client for interacting with your database
-export const createClient = () => {
-  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ''
-  const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
-  
-  return createSupabaseClient(supabaseUrl, supabaseAnonKey)
-}
